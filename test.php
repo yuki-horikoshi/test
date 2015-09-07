6c5c4244c21d0a9944113a9c9fa61bee15644dea
yuki-horikoshi <yuki.horikoshi.work@gmail.com>
2015-09-07 09:17:54 +0000

    test

--- test.php
+++ test.php
@@ -8,13 +8,12 @@ class Controller_Test extends Controller
 			echo 'string';
 		}
 
-		for ($i = 0; $i < 10; $i++)
-		{
+		for ($i = 0; $i < 10; $i++) {
 			echo $i;
 		}
 
 		# Macで追加
-		$b = 30;
+		$b = 33;
 		echo $b;
 
 		return ;
